8f40b1f85b3c91f4d9a61606ec5ed6e0367b5b2f
majesitcjake <jakelamarreid1@gmail.com>
2024-09-11 10:20:44 -0400

    credit alt and no middle text

---
+++ index.js
@@ -0,0 +1,88 @@
+const button = document.getElementById("button")
+const credit = document.getElementById("credit")
+const credit1 = document.getElementById("credit1")
+const credit2 = document.getElementById("credit2")
+const credit3 = document.getElementById("credit3")
+const credit4 = document.getElementById("credit4")
+const credit5 = document.getElementById("credit5")
+const credit6 = document.getElementById("credit6")
+
+    button.addEventListener("click", event => {
+        if(credit.style.visibility === "visible"){
+            credit.style.visibility = "hidden";
+        }
+
+        else{
+            credit.style.visibility = "visible"
+        }
+
+    });
+
+    button.addEventListener("click", event => {
+        if(credit1.style.visibility === "visible"){
+            credit1.style.visibility = "hidden";
+        }
+
+        else{
+            credit1.style.visibility = "visible"
+        }
+
+    });
+
+    button.addEventListener("click", event => {
+        if(credit2.style.visibility === "visible"){
+            credit2.style.visibility = "hidden";
+        }
+
+        else{
+            credit2.style.visibility = "visible"
+        }
+
+    });
+
+    button.addEventListener("click", event => {
+        if(credit3.style.visibility === "visible"){
+            credit3.style.visibility = "hidden";
+        }
+
+        else{
+            credit3.style.visibility = "visible"
+        }
+
+    });
+
+    button.addEventListener("click", event => {
+        if(credit4.style.visibility === "visible"){
+            credit4.style.visibility = "hidden";
+        }
+
+        else{
+            credit4.style.visibility = "visible"
+        }
+
+    });
+
+    button.addEventListener("click", event => {
+        if(credit5.style.visibility === "visible"){
+            credit5.style.visibility = "hidden";
+        }
+
+        else{
+            credit5.style.visibility = "visible"
+        }
+
+    });
+
+    button.addEventListener("click", event => {
+        if(credit6.style.visibility === "visible"){
+            credit6.style.visibility = "hidden";
+        }
+
+        else{
+            credit6.style.visibility = "visible"
+        }
+
+    });
+
+
+
